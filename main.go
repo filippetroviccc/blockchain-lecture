09c93f22caa67cf8db33cca74ca87315a9f60e82
Filip Petrovic <filip.petroviccc@gmail.com>
2019-07-21 11:52:25 +0200

    Part 2

--- main.go
+++ main.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	bc.AddBlock("Send 1 BTC to Filip")
+	bc.AddBlock("Send 2 more BTC to Petrovic")
 
 	for _, block := range bc.blocks {
 		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %x\n", block.Hash)
+		pow := NewProofOfWork(block)
+		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
 		fmt.Println()
 	}
 }
